ab11b3e6feb6c5025e295eeea33dbd4304c3bb41
Riza Fahmi <rizafahmi@gmail.com>
2021-02-27 22:22:46 +0700

    Dukungan offline dengan workbox

--- src/service-worker.js
+++ src/service-worker.js
@@ -50,7 +50,7 @@ registerRoute(
 // precache, in this case same-origin .png requests like those from in public/
 registerRoute(
   // Add in any other file extensions or routing criteria as needed.
-  ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'), // Customize this strategy as needed, e.g., by changing to CacheFirst.
+  ({ url }) => url.origin === self.location.origin && /\.(jpe?g|png|svg|ico)$/i.test(url.pathname), // Customize this strategy as needed, e.g., by changing to CacheFirst.
   new StaleWhileRevalidate({
     cacheName: 'images',
     plugins: [
